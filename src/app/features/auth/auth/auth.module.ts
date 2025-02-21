36e91a74e564aafe77aae6c673856e63e0bb0052
Daniel <daniel.gonzalez@beclevercorp.com>
2025-02-21 15:53:20 -0300

    agrego login

--- src/app/features/auth/auth/auth.module.ts
+++ src/app/features/auth/auth/auth.module.ts
@@ -3,9 +3,19 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { FormsModule } from '@angular/forms';
 import { LoginComponent } from '../login/login.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule, AuthRoutingModule, FormsModule],
+  imports: [
+    CommonModule,
+    AuthRoutingModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
 })
 export class AuthModule {}
